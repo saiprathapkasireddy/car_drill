66e863f6fcc31e61e9ef32325adda06a8643bb66
Sai Prathap <saiprathapkasireddy@gmail.com>
2024-02-19 20:35:49 -0500

    last car in inventory

--- problem2.js
+++ problem2.js
@@ -1,11 +1,15 @@
-let inventory=require("./data.js");
+//let inventory=require("./data.js");
 
 function lastCar(arr){
+    if(Array.isArray(arr)){
     for(let car of arr){
         if(car.id===arr.length){
             return `last car is a ${car.car_make} ${car.car_model}`
         }
     }
 }
+}
 
-console.log(lastCar(inventory));
+module.exports=lastCar;
+
+//console.log(lastCar(inventory));
