43149c5d97b42faf96790949bdb3b31ccb5afbcc
Sai Prathap <saiprathapkasireddy@gmail.com>
2024-02-19 20:38:52 -0500

    BMW and AUDI cars

--- problem6.js
+++ problem6.js
@@ -1,12 +1,15 @@
-let inventory=require("./data.js");
+
 
 function BMWANDAUDiCars(arr){
+    if(Array.isArray(arr)){
     let BMW_AUDI=[];
     for(let car of arr){
         BMW_AUDI.push(car.car_make);
     }
 
     return JSON.stringify(BMW_AUDI);
+}
+return [];
 };
 
-console.log(BMWANDAUDiCars(inventory));
+module.exports=BMWANDAUDiCars;
