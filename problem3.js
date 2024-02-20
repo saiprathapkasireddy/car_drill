e1ecbd72a7e2d2d1ce3ec471a62ba3bf4d46fe16
Sai Prathap <saiprathapkasireddy@gmail.com>
2024-02-19 20:36:37 -0500

    car models in alphabetical order

--- problem3.js
+++ problem3.js
@@ -1,12 +1,16 @@
-let inventory=require("./data.js")
+//let inventory=require("./data.js")
 
 function carModels(arr){
+    if(Array.isArray(arr)){
     let car_models=[];
     for(let car of arr){
         car_models.push(car.car_model);
     }
     car_models.sort();
     return car_models
+}
 };
 
-console.log(carModels(inventory));
+module.exports=carModels;
+
+//console.log(carModels(inventory));
